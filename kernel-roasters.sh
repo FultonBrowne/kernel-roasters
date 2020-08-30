a17c79b94fc4dbad4bcbf806f3577173c9229c12
fultons sabotage linux machine <gprlabs@outlook.com>
2020-08-30 21:18:30 +0000

    first commit

---
+++ kernel-roasters.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+echo "        /~~~~~~~~~~~~~~~~~~~/|";
+echo "       /              /######/ / |";
+echo "      /              /______/ /  |";
+echo "     ========================= /||";
+echo "     |_______________________|/ ||";
+echo "      |  \****/     \__,,__/    ||";
+echo "      |===\**/       __,,__     ||    kernel roasters -- Portland, Oregon";
+echo "      |______________\====/%____||";
+echo "      |   ___        /~~~~\ %  / |";
+echo "     _|  |===|===   /      \%_/  |";
+echo "    | |  |###|     |########| | /";
+echo "    |____\###/______\######/__|/";
+echo "    ~~~~~~~~~~~~~~~~~~~~~~~~~~";
+echo
+echo
+echo "Welcome to kernel roaters!!"
+echo "-------------------------------------------"
+echo "(A Generate a lite kernel config based on your hardware"
+echo "-------------------------------------------"
+echo "(B Generate a generic kerenel config with added support for your hardware"
+echo "-------------------------------------------"
+echo "(C Generate a kerenl config via a wizzard"
